97172b615861d09d4eb6ea4a035a8ef0812624b7
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2025-08-25 10:58:54 +0900

    Add functions: getComputerChoice, getHumanChoice, playRound
    getComputerChoice returns "rock" or "paper" or
    "scissors", depending on a random number.
    
    getHumanChoice calls prompt function that will ask
    the user "Rock, Paper, Scissors!"
    
    playRound hasn't been yet completed.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+function getComputerChoice() {
+  // get a random number between 0 - 2 and save the value in the variable "choice"
+  let choice = Math.floor(Math.random() * 3);
+
+  // return "rock" if choice equals 0
+  // return "paper" if choice equals 1
+  // return "scissors" if choice equals 2
+  return choice === 0 ? "rock" : choice === 1 ? "paper" : "scissors";
+}
+
+function getHumanChoice() {
+  // get an input from a player and save it in "choice"
+  let choice = prompt("Rock, Paper, Scissors!");
+
+  return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+  console.log(humanChoice);
+  console.log(computerChoice);
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+playRound(getHumanChoice().toLowerCase(), getComputerChoice());
