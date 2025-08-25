a8022e2d217f6281e270a8bb0a87923b8f39d6dd
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2025-08-25 12:33:14 +0900

    Complete playRound, Add playGame function
    If playRound is called, it declares a variable "result"
    that will store the result of a round, according to
    the "rock, paper, scissors" rule. It will log the
    result along with the brief process as well.
    
    When playGame is called, it declares humanScore and
    computerScore, initiating them with 0. It calls playRound
    5 times. Before every invoke, it re-assign player's
    choice and computer's choice so that each round results
    could differ. It takes the return value of the invoke,
    put in into a switch statement, and increment the player
    or computer score based on its result.
    After 5 times of round, it logs the final result,
    summing up every round.

--- script.js
+++ script.js
@@ -16,11 +16,117 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-  console.log(humanChoice);
-  console.log(computerChoice);
+  // declare a variable to store the result
+  let result;
+
+  // compare humanChoice with computerChoice by its cases
+  switch (humanChoice) {
+    case "rock":
+      switch (computerChoice) {
+        case "scissors":
+          // if the player wins, log the winning statement
+          console.log("You win! Rock beats scissors");
+          // store "win" in the "result" variable
+          result = "win";
+          break;
+        case "paper":
+          // if computer wins, log the losing statement
+          console.log("You lose! Paper beats rock.");
+          // store "lose" in the "result" variable
+          result = "lose";
+          break;
+        case "rock":
+          // if draws, log the draw statement
+          console.log("Draw!");
+          // store "draw" in the "result" variable
+          result = "draw";
+          break;
+      }
+      break;
+    case "paper":
+      switch (computerChoice) {
+        case "rock":
+          console.log("You win! Paper beats rock");
+          result = "win";
+          break;
+        case "scissors":
+          console.log("You lose! Scissors beats paper.");
+          result = "lose";
+          break;
+        case "paper":
+          console.log("Draw!");
+          result = "draw";
+          break;
+      }
+      break;
+    case "scissors":
+      switch (computerChoice) {
+        case "paper":
+          console.log("You win! Scissors beats paper");
+          result = "win";
+          break;
+        case "rock":
+          console.log("You lose! Rock beats paper.");
+          result = "lose";
+          break;
+        case "scissors":
+          console.log("Draw!");
+          result = "draw";
+          break;
+      }
+      break;
+  }
+
+  return result;
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+  // declare variables for scores
+  let humanScore = 0;
+  let computerScore = 0;
 
-playRound(getHumanChoice().toLowerCase(), getComputerChoice());
+  // repeat the round 5 times
+  for (let i = 0; i < 5; i++) {
+    // at each round, get new choices
+    // convert the player input to lower case so that it takes inputs case-insensitively
+    let humanChoice = getHumanChoice().toLowerCase();
+    let computerChoice = getComputerChoice();
+    switch (playRound(humanChoice, computerChoice)) {
+      // if the return value equals "win", increment humanScore by 1
+      case "win":
+        humanScore++;
+        break;
+      // if the return value equals "lose", increment computerScore by 1
+      case "lose":
+        computerScore++;
+        break;
+      // do nothing otherwise
+      default:
+        break;
+    }
+  }
+
+  console.log("Final result: \n");
+  // if humanScore is greater than computerScore, log final winning statement
+  // along with the score history
+  if (humanScore > computerScore) {
+    console.log("You win! :D");
+    console.log(`final result:
+    your score: ${humanScore}
+    computer score: ${computerScore}`);
+    // if computerScore is greater than humanScore, log final lose statement
+  } else if (computerScore > humanScore) {
+    console.log("You lose... :\\");
+    console.log(`final result:
+    your score: ${humanScore}
+    computer score: ${computerScore}`);
+  } else {
+    // log draw statement otherwise
+    console.log("It's a tie :b");
+    console.log(`final result:
+    your score: ${humanScore}
+    computer score: ${computerScore}`);
+  }
+}
+
+playGame();
